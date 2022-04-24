cf0bd7f8afc5d389420794446bce7c50cf4f90f5
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2022-04-24 23:19:50 +0200

    add login page

--- src/components/AllMovies.vue
+++ src/components/AllMovies.vue
@@ -2,16 +2,17 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div>
-      <!-- <router-link>
-        <h2>{{ movie.title }}</h2>
-      </router-link> -->
+      <!-- <router-link> -->
+      <h2>{{ movies }}</h2>
+      <!-- </router-link> -->
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
 export default {
-  name: "HelloWorld",
+  name: "AllMovies",
   props: {
     msg: String,
   },
@@ -20,26 +21,44 @@ export default {
       movies: [],
     };
   },
+  setup() {
+    // created() {
+    const url =
+      "https://api.themoviedb.org/3/search/movie?api_key=033b9feb10f6fc139ba6faa28567dfcf&query=Jack+Reacher";
+    // const url =
+    // "https://api.themoviedb.org/3/movie/157336?api_key=033b9feb10f6fc139ba6faa28567dfcf&append_to_response=videos,images";
+    // const url = "http://www.omdbapi.com?apikey=7c985542";
 
-  // created() {
-  //   const url = "http://www.omdbapi.com/?apikey=7c985542";
+    // Simple GET request using fetch
+    const searchMovies = async () => {
+      // const response = await fetch(`${url}&s=${title}`);
+      const response = await fetch(`${url}`);
+      const data = await response.json();
+      this.movies = reactive(data);
+      console.log("my movies " + this.movies);
+    };
+    searchMovies();
+  },
+  //   .then((response) => response.json())
+  //   .then((data) => (this.movies = data.Title));
+  // console.log("my movies " + this.movies);
   // },
 
-  mounted() {
-    const url = "http://www.omdbapi.com/?apikey=7c985542";
+  // mounted() {
+  // const url = "http://www.omdbapi.com/?apikey=7c985542";
 
-    async function getMovies() {
-      try {
-        let response = await fetch(url);
-        console.log("my movies " + response);
-        this.movies = response.json();
-      } catch (err) {
-        console.error(err);
-        // Handle errors here
-      }
-    }
-    getMovies();
-  },
+  // async function getMovies() {
+  //   try {
+  //     let response = await fetch(url);
+  //     console.log("my movies " + response);
+  //     this.movies = response.json();
+  //   } catch (err) {
+  //     console.error(err);
+  //     // Handle errors here
+  //   }
+  // }
+  // getMovies();
+  // },
 };
 </script>
 
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,102 @@
 <template>
   <h1>Login</h1>
+  <form @submit.prevent="handleSubmit">
+    <div class="input-container">
+      <div class="input-items">
+        <label>Email : </label>
+        <input
+          class="input"
+          type="email"
+          required
+          v-model="email"
+          placeholder="Enter your Email address"
+        />
+      </div>
+      <div class="input-items">
+        <label>Password : </label>
+        <input
+          type="password"
+          required
+          v-model="password"
+          class="input"
+          placeholder="Password"
+        />
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
+      </div>
+      <button>Login</button>
+    </div>
+  </form>
 </template>
 
-//
 <script>
-// export default {
-
-// }
-//
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      passwordError: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 characters long.";
+      console.log("submited");
+      // email = "",
+      // password = "",
+    },
+  },
+};
 </script>
+<style>
+/* form {
+  max-width: 420;
+  margin: 30px auto;
+  background: whitesmoke;
+  text-align: left;
+  padding: 40px;
+  border-radius: 20px;
+} */
+
+label {
+  color: rgb(38, 37, 37);
+  display: inline-block;
+  margin: 25px 0 5px;
+  font-size: 1em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+}
+.input-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: rgb(156, 184, 236);
+  margin: 100px 250px;
+  padding: 50px;
+  border-radius: 20px;
+}
+.input-items {
+}
+.input {
+  background-color: azure;
+  width: 300px;
+  margin: 30px;
+  height: 25px;
+  display: flex;
+  text-align: start;
+}
+button {
+  padding: 10px 25px;
+  border-radius: 10px;
+  background-color: dodgerblue;
+}
+.error {
+  color: rgb(53, 4, 4);
+  margin: 10px 0 10px 0;
+  font-size: 1em;
+  font-weight: bold;
+}
+</style>
